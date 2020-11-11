171bcc091528ea84697d4bfc1d6ff87d604ba2d1
Mohamed Shafee <mshafee1993@gmail.com>
2020-11-10 22:56:53 -0500

    initial commit

---
+++ webmaster/data/utils.py
@@ -0,0 +1,59 @@
+import csv
+from pathlib import Path
+
+root = "data/"
+
+def parseFile(filename):
+	"""Returns content of file as a list or dictionary
+	:param filename: filename (including file extension)
+	:return: a list containing the contents of each line if filename ends in .txt, otherwise a dict corresponding to the content if filename ends in .cvs 
+	"""
+	filepath = root + filename
+	try:
+		if filename.endswith('.txt'):
+			file = open(filepath)
+			return file.read().splitlines()
+		elif filename.endswith('.csv'):
+			reader = csv.reader(open(filepath))
+			result = {}
+			for row in reader:
+			    key = row[0]
+			    result[key] = row[1]
+			return result
+	except FileNotFoundError:
+		open(filepath,"w+").close()
+
+def searchFile(filename, term):
+	"""searches content of a .txt file for a term 
+	:param filename: filename (including file extension)
+	:param term: term to search for
+	:return: true is term is found in file, otherwise false 
+	"""
+	filepath = root + filename
+	file = open(filepath)
+	return term in file.read().splitlines()
+	file.close()
+
+
+def appendStringToFile(filename, string):
+	"""appends a string to the end of the .txt file on a new line 
+	:param filename: full filename including extension
+	:param term: string to append 
+	"""
+	filepath = root + filename
+	file = open(filepath, 'a+')
+	file.write(string + "\n")
+	file.close()
+
+def appendDictToFile(filename, dict):
+	filepath = root + filename
+	if Path(filepath).is_file():
+		file = open(filepath, 'a') 
+		fieldnames =  list(dict.keys())
+		dict_writer = csv.DictWriter(file, fieldnames = fieldnames)
+		dict_writer.writerow(dict)
+	else:
+		with open(filepath, 'w', newline='') as file:
+			fieldnames = list(dict.keys())
+			writer = csv.DictWriter(file, delimiter=',', lineterminator='\n',fieldnames=fieldnames)
+			writer.writeheader();
---
+++ webmaster/webmaster.py
@@ -0,0 +1,112 @@
+from data import utils
+from googlesearch import search
+from random import randint
+import time
+import hmac
+import base64
+from json import loads, dumps
+import hashlib
+import urllib
+
+MAX_DA = 70
+
+class ContactInformation:
+	def __init__(self, firstName, lastName, email, organization, domain, domainAuthority):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.email = email
+		self.organization = organization
+		self.domain = domain 
+		self.domainAuthority = domainAuthority
+	
+	def getContactDict(self):
+		return {"First Name": self.firstName,"Last Name": self.lastName,"email": self.email,"organization": self.organization,"domain": self.domain,"Domain Authority": self.domainAuthority}
+
+
+def getContactInformation(domain, HUNTER_API_KEY, domainAuthority):
+	print("Looking for contact information..")
+	uClient = urllib.request.urlopen("https://api.hunter.io/v2/domain-search?domain="+domain+"&api_key=" + HUNTER_API_KEY)
+	json = loads(uClient.read())	
+	uClient.close()
+	counter = 0
+	for email in json.get('data').get('emails'):
+		contact = ContactInformation(email.get('first_name'), email.get('last_name'),email.get('value'), json.get('data').get('organization'), json.get('data').get('domain'), domainAuthority)
+		if email.get('type') == 'personal':
+			if counter < 1:
+				utils.appendDictToFile("personal_contact_information.csv", contact.getContactDict())
+				print("Contact added to personal_contact_information.csv")
+				counter +=1
+			else:
+				utils.appendDictToFile("personal_contact_information_extra.csv", contact.getContactDict())
+				print("Contact added to personal_contact_information_extra.csv")
+		elif email.get('type') != 'personal':
+			utils.appendDictToFile("nonpersonal_contact_information.csv", contact.getContactDict())
+			print("Contact added to nonpersonal_contact_information.csv")
+
+def getDomainAuthority(domain, accessID, secretKey):	
+	try:
+		print("Connecting to Moz API...")
+		time.sleep(10)
+		expires = int(time.time() + 100)
+		stringToSign = accessID+"\n"+str(expires)
+		binarySignature = base64.b64encode(hmac.new(secretKey.encode(), stringToSign.encode(), hashlib.sha1).digest())
+		urlSafeSignature = urllib.parse.quote_plus(binarySignature.decode('utf-8'))
+		cols = "68719476736"
+		requestUrl = "http://lsapi.seomoz.com/linkscape/url-metrics/?Cols="+cols+"&AccessID="+accessID+"&Expires="+str(expires)+"&Signature="+urlSafeSignature
+		batchedDomains =[domain]
+		jsonencoded = dumps(batchedDomains)
+		binary_data = jsonencoded.encode('utf-8') 
+		req = urllib.request.Request(url=requestUrl, data=binary_data)
+		f = urllib.request.urlopen(req)
+		domainAuthority_string = ((f.read().decode('utf-8')))
+		if len(domainAuthority_string) == 12:
+			domainAuthority = domainAuthority_string[8:10]
+		elif len(domainAuthority_string) == 13:
+			domainAuthority = domainAuthority_string[8:11]
+		elif len(domainAuthority_string) == 11:
+			domainAuthority = domainAuthority_string[8]
+		return (domainAuthority)
+	except urllib.error.HTTPError as e:
+		raise SystemExit(e)
+
+def urlToDomain(url):
+		if (url[8:11] == 'www'):
+			domain = url.replace('https://www.', '').split('/', 1)[0]
+		else:
+			domain = url.replace('https://', '').split('/', 1)[0]
+		return domain
+
+def urlsToDomains(urls):
+	domains = []
+	for url in urls:
+		domains.append(urlToDomain(url))
+	return domains
+
+def google(keyword):	  
+	print("Getting Google search results for: " + keyword + "\n")
+	return list(search(keyword, tld='com', num=100, stop=100, pause=2))
+
+if __name__ == "__main__":
+	try:
+		credentials = utils.parseFile("credentials.csv")
+		MOZ_ACCESS_ID, MOZ_SECRET_KEY, HUNTER_API_KEY = credentials['MOZ_ACCESS_ID'], credentials['MOZ_SECRET_KEY'], credentials['HUNTER_API_KEY']
+	except:
+		MOZ_ACCESS_ID = input("Please enter your Moz API Access ID")
+		MOZ_SECRET_KEY = input("Please enter your Moz API Access ID")
+		HUNTER_API_KEY = input("Please enter your Hunter API key")
+	
+	keywords = utils.parseFile("keywords.txt")
+	for keyword in keywords:
+		domains = urlsToDomains(google(keyword))
+		for domain in domains:
+			if utils.searchFile("history.txt", domain) == False:
+				print("Domain: " + domain)
+				domainAuthority = int(getDomainAuthority(domain, MOZ_ACCESS_ID, MOZ_SECRET_KEY))
+				print("domainAuthority: " + str(domainAuthority))
+				if domainAuthority < MAX_DA:
+					getContactInformation(domain,HUNTER_API_KEY, domainAuthority)
+				else:
+					print("Domain authority score is too high. Skipping.")
+				utils.appendStringToFile("history.txt", domain)
+				print("________________________________")
+		time.sleep(randint(5,10))
